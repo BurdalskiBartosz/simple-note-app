c67d01d59840af237b4288659c9c65bd18dc4939
Bartosz <bartosz.burdalski@gmail.com>
2024-11-11 18:51:50 +0100

    Modify App

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./index.css";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import supabase from "./services/supabaseClient";
+import SideNav from "./components/SideNav";
+import { useAuth } from "./hooks/useAuth";
+import Notes from "./components/Notes";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const { session, user, signOut } = useAuth();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  if (!session) {
+    return (
+      <div className="container">
+        <Auth
+          supabaseClient={supabase}
+          providers={[]}
+          appearance={{ theme: ThemeSupa }}
+          theme="dark"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  } else if (user) {
+    return (
+      <section className="flex h-screen flex-col gap-y-1 bg-dark p-1">
+        <div className="flex h-full gap-1">
+          <SideNav />
+          <main className="grow h-full">
+            <button
+              onClick={() => signOut()}
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
+            >
+              LOGOUT
+            </button>
+
+            <Notes />
+          </main>
+        </div>
+      </section>
+    );
+  }
 }
-
-export default App
